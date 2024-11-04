f94559713bba1d10ea96b18ea052f8e9d9dd860b
garnetraven <corbinchristopher1@gmail.com>
2024-11-04 13:11:02 -0700

    update block color

--- entities/block.py
+++ entities/block.py
@@ -5,5 +5,5 @@ class Block(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
         self.image = pygame.Surface((TILESIZE * 2,TILESIZE * 3))
-        self.image.fill((255,0,255))
+        self.image.fill((0,0,255))
         self.rect = self.image.get_rect()
