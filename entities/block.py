bf36d4bc31361f9ac7c0867fbba0a660880c5b30
garnetraven <corbinchristopher1@gmail.com>
2024-11-04 12:54:34 -0700

    init block class

---
+++ entities/block.py
@@ -0,0 +1,9 @@
+import pygame
+from globals import *
+
+class Block(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.Surface((TILESIZE * 2,TILESIZE * 3))
+        self.image.fill((255,0,255))
+        self.rect = self.image.get_rect()
