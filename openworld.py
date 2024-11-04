20912d55e73f4ca1844657d19297b2652db549fe
garnetraven <corbinchristopher1@gmail.com>
2024-11-04 14:01:57 -0700

    init world gen function

--- openworld.py
+++ openworld.py
@@ -1,15 +1,43 @@
 import pygame
+from opensimplex import OpenSimplex
 from utils.state import State
 from entities.player import Player
+from entities.block import Block
+from globals import *
 
 class OpenWorld(State):
     def __init__(self, app):
         self.app = app
         self.screen = self.app.screen
 
-        self.player = Player()
+        self.sprites = pygame.sprite.Group() 
+        self.blocks = pygame.sprite.Group() 
 
-        self.sprites = pygame.sprite.Group(self.player) 
+        self.player = Player(self.blocks)
+
+        self.sprites.add(self.player)
+
+        self.gen_world()
+
+    def gen_world(self):
+        noise_generator = OpenSimplex(seed = 112340987613248) 
+        width = 100
+        height = 20
+        offset = 35
+
+        for x in range(width):
+            column = []
+            for y in range(height):
+                noise_value = noise_generator.noise2(x * 0.3, y * 0.2)
+                height_value = int((noise_value + 1) / 2 * height + 10)
+                column.append(height_value)
+
+                if height_value > 0:
+                    block = Block()
+                    block.rect.x = x * TILESIZE
+                    block.rect.y = (height - height_value + offset) * TILESIZE
+                    self.sprites.add(block)
+                    self.blocks.add(block)
 
     def update(self):
         self.sprites.update()
