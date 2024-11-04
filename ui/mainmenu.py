f5c6cc6f0c7cbd725da55b2dc43b02b90175eaf9
garnetraven <corbinchristopher1@gmail.com>
2024-11-04 10:57:31 -0700

    add statebutton to play

--- ui/mainmenu.py
+++ ui/mainmenu.py
@@ -1,7 +1,7 @@
 import pygame
 from globals import *
 from utils.state import State
-from utils.eventhandler import EventHandler
+from ui.components import StateButton
 
 class MainMenu(State):
     def __init__(self, app) -> None:
@@ -14,11 +14,17 @@ class MainMenu(State):
         self.title = "Pixel Odyssey"
         self.title_text = MainMenu.font.render(self.title, True, "white", None)
         self.title_rect = self.title_text.get_rect(center = (SCREENWIDTH // 2, SCREENHEIGHT // 2))
-
+        self.buttons = [
+            StateButton(self.screen, "Play", "open_world", (SCREENWIDTH // 2, SCREENHEIGHT // 2 + 50))
+        ]
 
     def update(self) -> None:
-        pass
+        for button in self.buttons:
+            if button.clicked():
+                button.use(self.app)
     
     def draw(self) -> None:
         self.screen.fill("black")
+        for button in self.buttons:
+            button.draw(self.screen)
         self.screen.blit(self.title_text, self.title_rect)
