7dc233ab7381359dd86fc5c14ea473278095254c
garnetraven <corbinchristopher1@gmail.com>
2024-11-03 19:10:48 -0700

    create state class for inheritance later on

---
+++ utils/state.py
@@ -0,0 +1,12 @@
+class State:
+    def __init__(self) -> None:
+        pass
+
+    def update(self) -> None:
+        pass
+
+    def draw(self) -> None:
+        pass
+
+    def close(self) -> None:
+        pass
