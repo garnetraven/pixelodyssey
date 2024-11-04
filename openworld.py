2863f6dfd511490ba83a32526cc34bf47d3b3ab0
garnetraven <corbinchristopher1@gmail.com>
2024-11-04 11:31:41 -0700

    add player

--- openworld.py
+++ openworld.py
@@ -1,13 +1,20 @@
+import pygame
 from utils.state import State
+from entities.player import Player
 
 class OpenWorld(State):
     def __init__(self, app):
         self.app = app
         self.screen = self.app.screen
 
+        self.player = Player()
+
+        self.all_sprites = pygame.sprite.Group(self.player)
+
     def update(self):
-        pass
+        self.all_sprites.update()
 
     def draw(self):
         self.screen.fill('lightblue')
+        self.all_sprites.draw(self.screen)
 
