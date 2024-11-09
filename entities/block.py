198e724bec118c74b67397f0698225149310dab0
garnetraven <corbinchristopher1@gmail.com>
2024-11-09 11:46:17 -0700

    update block class for textures and usability

--- entities/block.py
+++ entities/block.py
@@ -7,7 +7,19 @@ class Block(pygame.sprite.Sprite):
         self.name = name
         self.type = type
         self.in_groups = groups
-        self.image = image.copy
-        self.image = pygame.Surface((TILESIZE * 2,TILESIZE * 2))
-        self.image.fill((0,0,255))
-        self.rect = self.image.get_rect()
+        self.image = image.copy()
+        self.rect = self.image.get_rect(bottomleft = (position[0], position[1] + TILESIZE))
+        self.active = True
+        self.hardness = hardness
+        self.durability = durability
+
+    def break_block(self, strength) -> bool:
+        if strength >= self.hardness:
+            self.durability -= 1 * strength
+            if self.durability < 1:
+                self.active = False
+                self.kill()
+                return True
+        return False
+
+
